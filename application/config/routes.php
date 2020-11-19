780ca8d5faa5d29b7f10e30c1d138e9d380c5d75
Mickael-DP <m.dallepasqualine@gmail.com>
2020-11-19 17:03:59 +0100

    Tuto - 2-6

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'site';
+
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
 $route ['(:any)'] = 'site/$1';
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
+<?= doctype('html5') ?>
 <html>
   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= meta ("UTF-8","",'charset'); ?>
+    <?= meta("X-UA-Compatible", "IE=edge", 'http-equiv'); ?>
+    <?= meta("viewport", "width=device-width, initial-scale=1"); ?>
     <title><?= $title ?></title>
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -33,7 +37,8 @@
         </div>
         <div class="collapse navbar-collapse" id="main_nav">
           <ul class="nav navbar-nav">
-            <li><a href="/index.html">Accueil</a></li>
+          <li><?= anchor('index', "Accueil"); ?></li>
+          <li><?= anchor('contact', "Contact"); ?></li>
           </ul>
         </div>
       </div>
